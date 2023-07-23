ab1004e2598307b69d17115dfc05b784eaed3c9f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-07-24 00:21:57 +0100

    done for now

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -3,8 +3,10 @@ import { View, Text, TextInput, TouchableOpacity, Image, Flatlist } from 'react-
 import { useRouter } from 'expo-router'
 import styles from './welcome.style'
 import {icons, SIZES} from "../../../constants"
+const jobTypes = ["Full-Time", "Part-Time", "Contractor", "Remote"]
 const Welcome = () => {
   const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState('Full-Time')
   return (
     <View>
        <View style= {styles.container}>
@@ -12,12 +14,41 @@ const Welcome = () => {
         <Text style= {styles.welcomeMessage}>Find a Job</Text>
 
     </View>
-    <View style= {styles.searchContainer}>	
+    <View style = {styles.searchBtnImage}>	
     <View style= {styles.searchWrapper}>
       <TextInput
 style= {styles.searchInput}
+value = ""
+onChange={() => {}}
+placeholder='Enter a job title'
       />
-    </View></View>
+    </View>
+    <TouchableOpacity
+    style = {styles.searchBtn}
+    onPress={()=>{}}
+      >
+      <Image 
+      source={icons.search}/>
+      resizeMode="contain"
+      style = {styles.searchBtnImage}
+      </TouchableOpacity> </View>
+      <View style = {styles.tabsContainer}>
+        <Flatlist
+        data = {jobTypes}
+        renderItem = {({item}) => (
+          <TouchableOpacity style= {styles.tab(activeJobType, item)}
+           onPress={()=>{
+            setActiveJobType(item)
+            router.push(`/search/${item}`)
+           }}
+          >
+            <Text style ={styles.tab(activeJobType, item)}>{item}</Text>
+          </TouchableOpacity>        )}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+          />
+      </View>
     </View>
   )
 }
