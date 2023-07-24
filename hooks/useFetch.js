cc8ee0d271cbaaf6fa905e33333f48914a6627cf
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-07-24 08:55:58 +0100

    data fetch and error handling

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,9 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetch = (endpoint) =>{
-    const [data, setData] = useState([])
-    const [isLoading, setIsLoading]I= useState(false)
-    const [error, setError] = useState(null)
-    // go to 56:00 in the vid
-}
+const useFetch = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // go to 56:00 in the vid
+  //put key in env file
+  const options = {
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("Something's Wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+};
