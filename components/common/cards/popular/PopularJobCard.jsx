809548105f88320208b1a1cc8956a16aa831ed6f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-07-24 21:52:11 +0100

    added job card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-import styles from './popularjobcard.style'
+import styles from "./popularjobcard.style";
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ selectedJob, item, handleCarePress }) => {
   return (
     <View>
-      <Text>PopularJobCard</Text>
+      <TouchableOpacity
+        style={styles.container(selectedJob, item)}
+        onPress={() => handleCarePress(item)}
+      >
+      
+        <TouchableOpacity styles={styles.logoContainer(selectedJob, item)}>
+          <Image 
+          source={{uri: item.employer_logo}}
+          resizeMode="contain"
+          style = {styles.logoImage}
+          />
+        </TouchableOpacity>
+        <Text  style = {styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
