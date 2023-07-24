3f9b320560d70dc12a2b5ac7cac6015e7791709a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-07-24 08:36:51 +0100

    added popular jobs view component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
-import {useState}from 'react'
-import { View, Text, TextInput, TouchableOpacity, ActivityIndicator, Flatlist } from 'react-native'
-import PopularJobCard from "../../common/cards"
-import { useRouter } from 'expo-router'
+import React from "react";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  Flatlist,
+} from "react-native";
+import PopularJobCard from "../../common/cards";
+import { useRouter } from "expo-router";
 
-import {COLORS, SIZES} from "../../../constants"
+import { COLORS, SIZES } from "../../../constants";
 
-import styles from './popularjobs.style'
+import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
+  const router = useRouter;
+  const isLoading = false;
+  const error = false;
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View styles={styles.container}>
+      
+        <View styles={styles.header}>
+          <Text styles={styles.headerTitle}>Popular Jobs</Text>
+          <TouchableOpacity >
+            <Text styles={styles.headerBtn}>Show All</Text>
+          </TouchableOpacity>
+        </View>
+        <View styles={styles.container}>
+          {isLoading ? (
+            <ActivityIndicator size="large" colors = {COLORS.primary}/>
+          ) : error  ? (
+            <Text>Something went wrong</Text>
+          ):(
+          <Flatlist
+          data = {[1,2,3,4,5]} 
+          renderItem = {({item})=>{
+            <PopularJobCard
+            item = {item}
+            />
+          }}
+          keyExtractor = {item => item?.job}
+          contentContainerStyle = {{columnGap: SIZES.medium}}
+          horizontal
+          />)}
+        </View>
+      
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
